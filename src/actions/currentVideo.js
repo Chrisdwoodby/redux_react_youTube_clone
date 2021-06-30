d4c52a2bbd23b920e2892f0d1f10816e0986458b
chrisdwoodby <chrisdwoodby@gmail.com>
2021-06-30 18:55:21 -0500

    Made neccessary changes to type keys & search.js

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,6 +1,9 @@
-var changeVideo = (video) => ({
-
-  //TODO:  Return some action object to change the currently playing video.
+var changeVideo = ({video}) => ({
+  type: "CHANGE_VIDEO",
+  video: {video}
+  // console.log(video);
 });
 
 export default changeVideo;
+
+//TODO:  Return some action object to change the currently playing video.
--- src/actions/search.js
+++ src/actions/search.js
@@ -4,11 +4,15 @@ import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
-var handleVideoSearch = ({query}) => ({
-  type: "VIDEO_SEARCH",
-  payload: {query}
-});
+var handleVideoSearch = (q) => (
+  function thunk(dispatch) {
+    return $.get (`https://www.googleapis.com/youtube/v3/search`, ({videos}) => {
+      dispatch({type: "CHANGE_VIDEO_LIST", video: {videos}})
+    })
+  }
+);
 
 export default handleVideoSearch;
 
-//TODO:  Write an asynchronous action to handle a video search!
+// TODO:  Write an asynchronous action to handle a video search!
+// need to pass a function instead of an object
